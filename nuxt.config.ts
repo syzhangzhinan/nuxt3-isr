83067b1c9443b909c0313dfc741e9ad5404b3d8d
张志南 <syzhangzhinan@gmail.com>
2025-11-07 22:33:51 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
   nitro: {
     // preset: "node-server",
     // 初始预渲染一次（可让 crawler 自动发现，或手动列出）
-    // prerender: { routes: ["/time"] }
+    prerender: { routes: ["/time"] }
   },
   routeRules: {
-    "/time": { isr: 120 } // 每 120 秒允许再生一次静态产物
+    "/time": { isr: 3600 } // 每 120 秒允许再生一次静态产物
   },
   compatibilityDate: "2025-11-06"
 })
