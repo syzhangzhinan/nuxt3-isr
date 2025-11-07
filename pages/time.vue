c903439e0086eda414d48e601aea8d70e9955c69
张志南 <syzhangzhinan@gmail.com>
2025-11-07 17:31:48 +0800

    Update time.vue

--- pages/time.vue
+++ pages/time.vue
@@ -2,7 +2,7 @@
   <div style="font-family: sans-serif; padding: 20px">
     <h1>Nuxt 3 ISR Demo</h1>
     <p><strong>Server Time:</strong> {{ data?.time }}</p>
-    <p><em>Refreshed via ISR every 10 seconds</em></p>
+    <p><em>Refreshed via ISR every 120 seconds</em></p>
   </div>
 </template>
 
