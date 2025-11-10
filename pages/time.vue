ed0b7f58f230e11ec4b6f25b93c6e24b3a0cf329
mason <mason@legend.tech>
2025-11-10 10:32:25 +0800

    test isr

--- pages/time.vue
+++ pages/time.vue
@@ -2,7 +2,10 @@
   <div style="font-family: sans-serif; padding: 20px">
     <h1>Nuxt 3 ISR Demo</h1>
     <p><strong>Server Time:</strong> {{ data?.time }}</p>
-    <p><em>Refreshed via ISR every 10 seconds</em></p>
+    <p><em>Refreshed via ISR every 1800 seconds</em></p>
+    <p>
+      <em>当前时间：{{ new Date().toLocaleString() }}</em>
+    </p>
   </div>
 </template>
 
