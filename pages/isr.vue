c802c23e546a28c948adf03adb8fc5f7893a56e9
mason <mason@legend.tech>
2025-11-10 14:12:58 +0800

    test isr

--- pages/isr.vue
+++ pages/isr.vue
@@ -4,11 +4,16 @@
     <p><strong>Server Time:</strong> {{ data?.time }}</p>
     <p><em>Refreshed via ISR every 120 seconds</em></p>
     <p>
-      <em>当前时间：{{ new Date().toLocaleString() }}</em>
+      <em>当前时间：{{ serverTime }}</em>
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
 const { data } = await useAsyncData("server-time", () => $fetch("/api/time"))
+
+const serverTime: any = ref("")
+onMounted(() => {
+  $fetch("/api/time").then((data) => (serverTime.value = data.time))
+})
 </script>
