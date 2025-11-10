7d8e785eb94267b340663064760b57ddcc08bfa1
mason <mason@legend.tech>
2025-11-10 16:59:49 +0800

    test isr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,13 @@ export default defineNuxtConfig({
   nitro: {
     // preset: "node-server",
     // 初始预渲染一次（可让 crawler 自动发现，或手动列出）
-    prerender: { routes: ["/swr"] }
+    // prerender: { routes: ["/swr"] }
     // storage: {
     //   cache: { driver: "fs", base: "./.nitro/cache" } // 或平台支持的共享存储
     // }
   },
   routeRules: {
-    "/isr": { isr: 120 }, // 每 120 秒允许再生一次静态产物
+    //   "/isr": { isr: 120 }, // 每 120 秒允许再生一次静态产物
     "/swr": { swr: 120 }
   },
   compatibilityDate: "2025-11-06"
